49fc9d9e97391b0834777e08a25326355ecff360
Cedric Segura <cedricsegurafr@gmail.com>
2024-05-22 12:14:29 +0200

    branch validation message added

--- scripts/husky/hooks/check_branch_naming.sh
+++ scripts/husky/hooks/check_branch_naming.sh
@@ -5,9 +5,12 @@ valid_branch_regex='^((feature|fix)\/[a-zA-Z0-9\-]+)$'
 
 message="Branch names in this project must adhere to this contract: $valid_branch_regex. Your commit will be rejected. You should rename your branch to a valid name and try again."
 
-if [[ ! $local_branch_name =~ $valid_branch_regex ]]; then
+if [[ ! $local_branch_name =~ $valid_branch_regex ]]; 
+then
     echo "$message"
     exit 1
+else 
+    echo "Branch name validated"
 fi
 
 exit 0
