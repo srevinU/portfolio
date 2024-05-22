7c1119e9166d823f30b4d03a00ee9fee8353c5a9
Cedric Segura <cedricsegurafr@gmail.com>
2024-05-22 19:36:49 +0200

    husky update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,14 @@
 import headerMenu from "../utils/struc/headerMenu";
-import "../css/components/Header.css"
+import "../css/components/Header.css";
 
 function Header(): JSX.Element {
   return (
     <div className="Header">
-        { headerMenu.map(menuItem => 
-            <a className="MenuItem" href={ menuItem.ref } key={ menuItem.key }> 
-                { menuItem.label } 
-            </a>) 
-        }
+      {headerMenu.map((menuItem) => (
+        <a className="MenuItem" href={menuItem.ref} key={menuItem.key}>
+          {menuItem.label}
+        </a>
+      ))}
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,7 @@
 import "../css/pages/Home.css";
 
 function Home(): JSX.Element {
-  return (
-    <div className="Home">
-      {/* <p>Home page</p> */}
-    </div>
-  );
+  return <div className="Home">{/* <p>Home page</p> */}</div>;
 }
 
 export default Home;
