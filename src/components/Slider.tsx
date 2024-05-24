a08a1502aa1a294b170e299b240b9e2d7dcf0626
Cedric Segura <cedricsegurafr@gmail.com>
2024-05-24 15:23:06 +0200

    Logo project details added

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,47 +1,49 @@
-import sliderProjects from "../utils/struc/sliderProjects";
+import { sliderProjects } from "../utils/struc/sliderProjects";
 import "../css/components/Slider.css";
 import { useState } from "react";
-import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { Techno } from "../utils/struc/sliderProjects";
 
 function Slider(): JSX.Element {
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  const nextSlide = (): void => {
-    setCurrentSlide(
-      currentSlide === sliderProjects.length - 1 ? 0 : currentSlide + 1,
-    );
-  };
-
-  const previousSlide = (): void => {
-    setCurrentSlide(
-      currentSlide === 0 ? sliderProjects.length - 1 : currentSlide - 1,
-    );
-  };
+  const [currentSlide, setCurrentSlide] = useState<number>(0);
 
   const selectProject = (index: number): void => {
     setCurrentSlide(index);
   };
 
   return (
-    <section className="slider">
-      {/* <IoIosArrowBack className='left-arrow' onClick={previousSlide} /> */}
-      {sliderProjects.map((project) => (
-        <div
-          className={project.index === currentSlide ? "slide active" : "slide"}
-          key={project.key}
-        >
-          {
+    <div className="slider">
+      <section className="image_wrapper">
+        {sliderProjects.map((project) => (
+          <div
+            className={
+              project.index === currentSlide ? "slide active" : "slide"
+            }
+            key={project.key}
+          >
             <img
               className="slide_image"
               src={project.src}
               alt={project.headline}
-              onClick={() => selectProject(project.index)}
+              onMouseOver={() => selectProject(project.index)}
             />
-          }
-        </div>
-      ))}
-      {/* <IoIosArrowForward className='right-arrow' onClick={nextSlide} /> */}
-    </section>
+            <a className="project_link" href="/projects">
+              Click me
+            </a>
+          </div>
+        ))}
+      </section>
+
+      <section className="details_wrapper">
+        {sliderProjects[currentSlide].technos.map((techno: Techno) => (
+          <img
+            className="logo"
+            alt={techno.name}
+            src={techno.src}
+            key={techno.key}
+          />
+        ))}
+      </section>
+    </div>
   );
 }
 
--- src/utils/struc/sliderProjects.ts
+++ src/utils/struc/sliderProjects.ts
@@ -6,6 +6,7 @@ type Project = {
   headline: string;
   button: Button;
   src: string;
+  technos: Array<Techno>;
 };
 
 type Button = {
@@ -13,13 +14,35 @@ type Button = {
   href: string;
 };
 
-const sliderProjects: SliderProjects = [
+export type Techno = {
+  key: string;
+  index: number;
+  name: string;
+  src: string;
+};
+
+export const sliderProjects: SliderProjects = [
   {
     key: "1",
     index: 0,
     headline: "Project 1",
     button: { label: "View Project", href: "/projects_1" },
     src: "https://images.unsplash.com/photo-1546768292-fb12f6c92568?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    technos: [
+      { key: "1", index: 0, name: "React", src: "/assets/logos/react-2.svg" },
+      {
+        key: "2",
+        index: 1,
+        name: "Node.js",
+        src: "/assets/logos/nodejs-1.svg",
+      },
+      {
+        key: "3",
+        index: 2,
+        name: "TypeScript",
+        src: "/assets/logos/typescript.svg",
+      },
+    ],
   },
   {
     key: "2",
@@ -27,14 +50,47 @@ const sliderProjects: SliderProjects = [
     headline: "Project 2",
     button: { label: "View Project", href: "/projects_2" },
     src: "https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
+    technos: [
+      {
+        key: "4",
+        index: 0,
+        name: "Vue.js",
+        src: "/assets/logos/Vue.js_Logo_2.svg",
+      },
+      {
+        key: "5",
+        index: 1,
+        name: "Node.js",
+        src: "/assets/logos/nodejs-1.svg",
+      },
+    ],
   },
   {
-    key: "3",
+    key: "6",
     index: 2,
     headline: "Project 3",
     button: { label: "View Project", href: "/project_3" },
     src: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/fashion.jpg",
+    technos: [
+      { key: "6", index: 0, name: "React", src: "/assets/logos/react-2.svg" },
+      {
+        key: "7",
+        index: 1,
+        name: "Node.js",
+        src: "/assets/logos/nodejs-1.svg",
+      },
+      {
+        key: "8",
+        index: 2,
+        name: "TypeScript",
+        src: "/assets/logos/typescript.svg",
+      },
+      {
+        key: "9",
+        index: 2,
+        name: "TypeScript",
+        src: "/assets/logos/docker-3.svg",
+      },
+    ],
   },
 ];
-
-export default sliderProjects;
