90a3395a30a81fe2924e18a3abeaee04d2ae76aa
Cedric Segura <cedricsegurafr@gmail.com>
2024-05-26 21:38:13 +0200

    Maitenance structure added

--- src/pages/Maintenance.tsx
+++ src/pages/Maintenance.tsx
@@ -1,20 +1,14 @@
 import { IoMdSettings } from "react-icons/io";
 import "../css/pages/Maintenance.css";
+import maintenanceContent from "../utils/struc/maintenaceContent";
 
 function Maintenance(): JSX.Element {
   return (
     <div className="maintenance_page">
       <section className="maintenance_bg">
         <IoMdSettings size={75} />
-        <h1>
-          We're currently undergoing maintenance <br />
-          to improve your experience.
-        </h1>
-        <p>
-          Please bear with us while we make these enhancements. We'll be back
-          shortly.
-        </p>
-        <p>Thank you for your patience!</p>
+        <h1>{maintenanceContent.title}</h1>
+        <p>{maintenanceContent.body}</p>
       </section>
     </div>
   );
