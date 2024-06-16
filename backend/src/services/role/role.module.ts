99b6bab005e89cfa65e4287b4930f6e02e964a73
Cedric Segura <cedricsegurafr@gmail.com>
2024-06-16 16:44:55 +0200

    cleanUp

--- backend/src/services/role/role.module.ts
+++ backend/src/services/role/role.module.ts
@@ -5,19 +5,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Role, RoleSchema } from './shemas/role.schema';
 import { UserModule } from '../user/user.module';
 import { RoleAdminStrategy } from 'src/strategies/roles.strategy';
-import { JwtStrategy } from 'src/strategies/jwt.strategy';
-import { User, UserSchema } from '../user/schemas/user.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([
-      { name: Role.name, schema: RoleSchema },
-      { name: User.name, schema: UserSchema },
-    ]),
+    MongooseModule.forFeature([{ name: Role.name, schema: RoleSchema }]),
     forwardRef(() => UserModule),
   ],
   controllers: [RoleController],
-  providers: [RoleService, JwtStrategy, RoleAdminStrategy],
+  providers: [RoleService, RoleAdminStrategy],
   exports: [RoleService],
 })
 export class RoleModule {}
