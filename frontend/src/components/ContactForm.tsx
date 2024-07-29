ce79a43c2ce8006d4f340651bc053491429a90a7
Jenkins <cedricsegurafr@gmail.com>
2024-07-29 11:54:41 +0200

    hooks isolation and smtp handle error

--- frontend/src/components/ContactForm.tsx
+++ frontend/src/components/ContactForm.tsx
@@ -1,11 +1,8 @@
-import { useEffect, useRef, useState } from "react";
 import { LanguageT } from "../utils/types/general";
-import { ContactsFormT, ContactInputsFormT } from "../utils/types/ContactForm";
+import { ContactsFormT } from "../utils/types/ContactForm";
 import "../style/components/ContactForm.css";
-import SmtpService from "../webServices/Smtp";
 import Popin from "./Popin";
-import { AxiosResponse } from "axios";
-import { PopInT } from "../utils/types/PopIn";
+import useContactFormsHooks from "../hooks/contactFroms";
 
 function ContactForm({
   contactForm,
@@ -16,92 +13,8 @@ function ContactForm({
   language: LanguageT;
   isMobile: boolean;
 }): JSX.Element {
-  const formRef = useRef<HTMLFormElement>(null);
-
-  const inputsForm: ContactInputsFormT = {
-    name: "",
-    email: "",
-    message: "",
-  };
-
-  const [contactInputs, setContactInputs] =
-    useState<ContactInputsFormT>(inputsForm);
-
-  const handleChange = (
-    e:
-      | React.ChangeEvent<HTMLInputElement>
-      | React.ChangeEvent<HTMLTextAreaElement>,
-  ): void => {
-    setContactInputs({ ...contactInputs, [e.target.name]: e.target.value });
-  };
-
-  const clearContactInputs = (): void => {
-    formRef.current?.reset();
-  };
-
-  const [popIn, setPopIn] = useState<PopInT>({
-    active: false,
-    message: "",
-    statusCode: 200,
-  });
-
-  const [loading, setLoading] = useState<boolean>(false);
-
-  useEffect(() => {
-    let timeOutId: NodeJS.Timeout | null = null;
-    if (popIn.active) {
-      timeOutId = setTimeout((): void => {
-        setPopIn({
-          active: false,
-          message: "",
-          statusCode: 200,
-        });
-      }, 5000);
-    }
-    return (): void => {
-      if (timeOutId) return clearTimeout(timeOutId);
-    };
-  }, [popIn]);
-
-  const handlePopin = (result: AxiosResponse): void => {
-    if (result.status) {
-      setPopIn({
-        active: true,
-        message: result.data.message,
-        statusCode: result.status,
-      });
-    } else {
-      setPopIn({
-        active: true,
-        message: "Unable to send you email, pleader try later...",
-        statusCode: 500,
-      });
-    }
-  };
-
-  const isFormValid = (): boolean => {
-    return (
-      contactInputs.name !== "" &&
-      contactInputs.email !== "" &&
-      contactInputs.message !== ""
-    );
-  };
-
-  const handleSubmit = async (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-  ): Promise<void> => {
-    if (isFormValid()) {
-      e.preventDefault();
-      setLoading(true);
-      const result = await SmtpService.sendEmail(
-        `${contactInputs.name} - ${contactInputs.email}`,
-        contactInputs.message,
-      );
-      handlePopin(result);
-      clearContactInputs();
-      setLoading(false);
-    }
-  };
+  const { formRef, handleChange, handleSubmit, loading, popIn } =
+    useContactFormsHooks();
 
   return (
     <div className="contactForm">
--- frontend/src/webServices/Smtp.ts
+++ frontend/src/webServices/Smtp.ts
@@ -1,24 +1,19 @@
-import axios, { AxiosResponse } from "axios";
+import { AxiosError, AxiosResponse } from "axios";
 import WebService from "./WebService";
 
 export default class SmtpService extends WebService {
   static async sendEmail(
     from: string,
     message: string,
-  ): Promise<AxiosResponse> {
+  ): Promise<AxiosResponse | AxiosError> {
     try {
       return await this.axiosInstance.post("/email/", {
         from_: from,
         message: message,
       });
     } catch (error) {
-      if (axios.isAxiosError(error)) {
-        console.log(error.status);
-        console.error(error.response);
-        throw error;
-      } else {
-        throw new Error(error as string);
-      }
+      console.error(error);
+      return error as AxiosError;
     }
   }
 }
