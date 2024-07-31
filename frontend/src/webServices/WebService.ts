3d5b01d8c02019210fd99f36e63ef7ab5ce86945
Jenkins <cedricsegurafr@gmail.com>
2024-07-31 16:05:10 +0200

    smtp service update

--- frontend/src/webServices/WebService.ts
+++ frontend/src/webServices/WebService.ts
@@ -5,10 +5,12 @@ export default class WebService {
     baseURL: process.env.REACT_APP_SMTP_API_URL,
     withCredentials: false,
     headers: {
-      "Content-Type": process.env.CONTENT_TYPE,
-      Accept: "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Content-Type": process.env.REACT_APP_CONTENT_TYPE,
+      Accept: process.env.REACT_APP_ACCEPT,
+      "Access-Control-Allow-Origin":
+        process.env.REACT_APP_ACCESS_CONTROL_ALLOW_ORIGIN,
+      "Access-Control-Allow-Methods":
+        process.env.REACT_APP_ACCESS_CONTROL_ALLOW_METHODS,
     },
   });
 }
