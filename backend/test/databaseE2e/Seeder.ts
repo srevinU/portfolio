edf27e44f49c38decfb80044f08317336dea2eef
Jenkins <cedricsegurafr@gmail.com>
2024-08-27 11:58:28 +0200

    Referencials test e2e

--- backend/test/databaseE2e/Seeder.ts
+++ backend/test/databaseE2e/Seeder.ts
@@ -1,79 +1,125 @@
 import { Mongoose, Types } from 'mongoose';
+import Service from '../../src/services/service';
 import { User, UserSchema } from '../../src/services/user/schemas/user.schema';
 import { Role, RoleSchema } from '../../src/services/role/shemas/role.schema';
 import {
   AdminConfig,
   AdminConfigSchema,
 } from '../../src/services/admin/adminConfig/schemas/adminConfig.schema';
+import {
+  Techno,
+  TechnoSchema,
+} from '../../src/services/referencials/technos/schemas/techno.schema';
+import {
+  Language,
+  LanguageSchema,
+} from '../../src/services/referencials/languages/schemas/language.schema';
 import { userAdmin } from '../payloads/user';
 import { roleAdmin } from '../payloads/role';
 import { adminConfigTest as AdminConfigPayload } from '../payloads/adminConfig';
+import { Technos } from '../payloads/technos';
+import { Languages } from '../payloads/languages';
 
-export default class Seeder {
+export default class Seeder extends Service {
   private mongodb: Mongoose;
 
   constructor(mongodb: Mongoose) {
+    super('Seeder');
     this.mongodb = mongodb;
   }
 
   private async generateUserSeeder(role: Types.ObjectId | null) {
-    try {
-      const userCount = await this.mongodb
-        .model(User.name, UserSchema)
-        .where('email', userAdmin.email)
-        .countDocuments({});
-      if (userCount === 0) {
-        await this.mongodb.model(User.name, UserSchema).create({
-          name: userAdmin.name,
-          email: userAdmin.email,
-          roles: [role],
-          password: userAdmin.password,
-        });
-      }
-    } catch (error) {
-      console.error('Error creating user seeder', error);
-    }
+    return this.mongodb
+      .model(User.name, UserSchema)
+      .where('email', userAdmin.email)
+      .countDocuments({})
+      .then((count) => {
+        if (count === 0) {
+          return this.mongodb.model(User.name, UserSchema).create({
+            name: userAdmin.name,
+            email: userAdmin.email,
+            roles: [role],
+            password: userAdmin.password,
+          });
+        }
+        return null;
+      })
+      .then((data) => this.catcher(data));
   }
 
   private async generateRoleSeeder(): Promise<Role> {
-    let role: Role;
-    try {
-      role = await this.mongodb
-        .model(Role.name, RoleSchema)
-        .findOne({ name: roleAdmin.name });
-      if (!role) {
-        role = await this.mongodb.model(Role.name, RoleSchema).create({
-          name: roleAdmin.name,
-          description: roleAdmin.description,
-          permissions: roleAdmin.permissions,
-        });
-      }
-    } catch (error) {
-      console.error('Error creating role seeder', error);
-    }
-    return role;
+    return this.mongodb
+      .model(Role.name, RoleSchema)
+      .findOne({ name: roleAdmin.name })
+      .then((role) => {
+        if (!role) {
+          return this.mongodb.model(Role.name, RoleSchema).create({
+            name: roleAdmin.name,
+            description: roleAdmin.description,
+            permissions: roleAdmin.permissions,
+          });
+        }
+        return role;
+      })
+      .then((data) => this.catcher(data));
   }
 
   private async generateAdminConfigSeeder(): Promise<AdminConfig> {
-    let adminConfig: AdminConfig;
-    try {
-      adminConfig = await this.mongodb
-        .model(AdminConfig.name, AdminConfigSchema)
-        .findOne({ name: AdminConfigPayload._id });
-      if (!adminConfig) {
-        adminConfig = await this.mongodb
-          .model(AdminConfig.name, AdminConfigSchema)
-          .create(AdminConfigPayload);
-      }
-    } catch (error) {
-      console.error('Error creating admin configuration seeder', error);
-    }
-    return adminConfig;
+    return this.mongodb
+      .model(AdminConfig.name, AdminConfigSchema)
+      .findOne({ name: AdminConfigPayload._id })
+      .then((adminConfig) => {
+        if (!adminConfig) {
+          return this.mongodb
+            .model(AdminConfig.name, AdminConfigSchema)
+            .create(AdminConfigPayload);
+        }
+        return adminConfig;
+      })
+      .then((data) => this.catcher(data));
+  }
+
+  private async generateTechnosRefSeeder(): Promise<Array<Techno>> {
+    return this.mongodb
+      .model(Techno.name, TechnoSchema)
+      .find()
+      .then((technos) => {
+        if (technos.length === 0) {
+          return Promise.all(
+            Technos.map((techno) =>
+              this.mongodb.model(Techno.name, TechnoSchema).create(techno),
+            ),
+          );
+        }
+        return technos;
+      })
+      .then((data) => this.catcher(data));
+  }
+
+  private async generateLanguagesRefSeeder(): Promise<Array<Techno>> {
+    return this.mongodb
+      .model(Language.name, LanguageSchema)
+      .find()
+      .then((languages) => {
+        if (languages.length === 0) {
+          return Promise.all(
+            Languages.map((language) =>
+              this.mongodb
+                .model(Language.name, LanguageSchema)
+                .create(language),
+            ),
+          );
+        }
+        return languages;
+      })
+      .then((data) => this.catcher(data));
   }
 
   public async generateSeeders(): Promise<void> {
     this.generateRoleSeeder()
       .then((role) => this.generateUserSeeder(role._id))
-      .then(() => this.generateAdminConfigSeeder());
+      .then(() => this.generateAdminConfigSeeder())
+      .then(() => this.generateTechnosRefSeeder())
+      .then(() => this.generateLanguagesRefSeeder());
   }
 }
--- backend/test/specs/languages.e2e-spec.ts
+++ backend/test/specs/languages.e2e-spec.ts
@@ -1,13 +1,15 @@
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { roleTest } from './payloads/role';
-import { MongoTestApp } from './databaseE2E/MongoTestApp';
+import { MongoTestApp } from '../databaseE2E/MongoTestApp';
+import { Languages } from '../payloads/languages';
+import { Language } from 'src/services/referencials/languages/entities/language.entity';
 
 let token: string;
 let testApp: INestApplication;
 let MongoTestAppInstance: MongoTestApp;
+let newRecordCreated: Language;
 
-describe('Role (e2e)', () => {
+describe('Languages Referencials (e2e)', () => {
   beforeAll(async (): Promise<void> => {
     MongoTestAppInstance = MongoTestApp.getInstance();
     await MongoTestAppInstance.start();
@@ -26,44 +28,65 @@ describe('Role (e2e)', () => {
         password: 'adminTest',
       })
       .end((err: Error, res: request.Response) => {
+        if (err) return done(err);
         expect(res.status).toBe(201);
         expect(res.body.status).toBe('OK');
         expect(res.body.message).toBe('Login successful');
         expect(res.header['set-cookie']).toBeDefined();
         expect(res.header['set-cookie'].length).toBeGreaterThan(0);
-        if (err) return done(err);
         token = res.get('set-cookie');
         done();
       });
   });
 
-  it('Create role', (done: jest.DoneCallback): void => {
+  it('Create languages', (done: jest.DoneCallback): void => {
     request(testApp.getHttpServer())
-      .post('/api/role')
+      .post('/api/referencials/languages')
       .set('Accept', 'application/json')
       .set('cotent-type', 'application/json')
       .set('Cookie', token)
-      .send(roleTest)
+      .send(Languages[0])
       .end((err: Error, res: request.Response) => {
-        expect(res.status).toBe(201);
-        expect(res.body.name).toBe(roleTest.name);
-        expect(res.body.description).toBe(roleTest.description);
-        expect(res.body.permissions).toStrictEqual(roleTest.permissions);
         if (err) return done(err);
+        expect(res.status).toBe(201);
+        expect(res.body.name).toBe(Languages[0].name);
+        expect(res.body.src).toBe(Languages[0].src);
+        newRecordCreated = res.body;
         done();
       });
   });
 
-  it('Delete role', (done: jest.DoneCallback): void => {
+  it('Find languages', (done: jest.DoneCallback): void => {
     request(testApp.getHttpServer())
-      .delete(`/api/role/${roleTest.name}`)
+      .get('/api/referencials/languages')
       .set('Accept', 'application/json')
       .set('cotent-type', 'application/json')
       .set('Cookie', token)
       .end((err: Error, res: request.Response) => {
-        expect(res.status).toBe(200);
-        expect(res.body.deletedCount).toBe(1);
         if (err) return done(err);
+        expect(res.status).toBe(200);
+        expect(res.body.length).toBeGreaterThan(0);
+        expect(res.body.length).toBe(Languages.length + 1);
+        done();
+      });
+  });
+
+  it('Update languages', (done: jest.DoneCallback): void => {
+    const recordToUpdate: Language = {
+      ...newRecordCreated,
+      name: 'updated name',
+    };
+    request(testApp.getHttpServer())
+      .patch('/api/referencials/languages')
+      .set('Accept', 'application/json')
+      .set('cotent-type', 'application/json')
+      .set('Cookie', token)
+      .send(recordToUpdate)
+      .end((err: Error, res: request.Response) => {
+        if (err) return done(err);
+        expect(res.status).toBe(200);
+        expect(res.body._id).toBe(recordToUpdate._id);
+        expect(res.body.name).toStrictEqual(recordToUpdate.name);
         done();
       });
   });
