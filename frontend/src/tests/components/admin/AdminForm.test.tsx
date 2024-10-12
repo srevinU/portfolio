aa7a584b704d967664ca8a9e6f78810dc8afef0d
Jenkins <cedricsegurafr@gmail.com>
2024-10-12 12:08:56 +0200

    Experience clean up

--- frontend/src/tests/components/admin/AdminForm.test.tsx
+++ frontend/src/tests/components/admin/AdminForm.test.tsx
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/react";
 import AdminFrom from "../../../components/admin/AdminForm";
 describe("AdminForm", () => {
-  it("Renders correctly", async () => {
-    await render(<AdminFrom />);
+  it("Renders correctly", () => {
+    render(<AdminFrom />);
   });
 });
--- frontend/src/webServices/AdminConfig.ts
+++ frontend/src/webServices/AdminConfig.ts
@@ -4,24 +4,14 @@ export default class AdminConfig extends WebService {
   static async get(_id: string) {
     return this.axiosInstance
       .get(`${process.env.REACT_APP_BACKEND_SUB_NAME}/adminConfig/${_id}`)
-      .then((response) => {
-        return response.data;
-      })
-      .catch((error) => {
-        console.error(error);
-        return error;
-      });
+      .then((response) => response.data)
+      .catch((error) => console.error(error));
   }
 
   static async update(data: any) {
     return this.axiosInstance
       .patch(`${process.env.REACT_APP_BACKEND_SUB_NAME}/adminConfig`, data)
-      .then((response) => {
-        return response.data;
-      })
-      .catch((error) => {
-        console.error(error);
-        return error;
-      });
+      .then((response) => response.data)
+      .catch((error) => console.error(error));
   }
 }
